f0351ef17203be909583a6e3abf4dd8b5d9d04b1
nini gedeshuri <111783935+astarta16@users.noreply.github.com>
2024-05-13 16:18:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -49,8 +49,7 @@ class Game:
         # self.screen.fill('black')
         self.object_renderer.draw()
         self.weapon.draw()
-        # self.map.draw()
-        # self.player.draw()
+       
 
     def check_events(self):
         self.global_trigger = False
