f78b4c9967d0a253699700065363c2e62ee9797b
nini gedeshuri <111783935+astarta16@users.noreply.github.com>
2024-05-23 10:30:24 +0400

    Update raycasting.py

--- raycasting.py
+++ raycasting.py
@@ -60,7 +60,6 @@ class RayCasting:
                 y_hor += dy
                 depth_hor += delta_depth
 
-            # verticals
             x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
 
             depth_vert = (x_vert - ox) / cos_a
